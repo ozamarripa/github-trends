69160efd3edf6f61638da3104d7df60b1cdbb969
Omar Zamarripa <omar.zamarripa@gmail.com>
2017-08-19 09:34:41 +0100

    Add styles and file loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+import "./styles.css"
+import Icon from "./bg.jpg"
 
 function component() {
-  var element = document.createElement("div")
-  element.innerHTML = "Hello!!"
+  var wrapper = document.createElement("div")
+  wrapper.classList.add("flexcontainer")
+  var element = document.createElement("p")
+  element.innerHTML = "Github Trends"
+  element.classList.add("content")
+  wrapper.appendChild(element)
 
-  return element
+  var myIcon = new Image();
+  myIcon.src = Icon
+
+  // wrapper.appendChild(myIcon)
+  return wrapper
 }
 
 document.body.appendChild(component())
