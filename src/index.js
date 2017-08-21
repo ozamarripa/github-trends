44eb31d22c6c50a606a2ed097dfd0f2198c9dc6e
Omar Zamarripa <omar.zamarripa@gmail.com>
2017-08-21 07:20:40 +0100

    Add http-server

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ function component() {
   var wrapper = document.createElement("div")
   wrapper.classList.add("flexcontainer")
   var element = document.createElement("p")
+
   element.innerHTML = "Github Trends"
   element.classList.add("content")
+
   wrapper.appendChild(element)
 
   var myIcon = new Image();
