6dd38eac7a4c7a00454cdd112e721fc4a9925321
Omar Zamarripa <omar.zamarripa@gmail.com>
2017-08-31 07:34:50 +0100

    Add prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import './styles.css'
+import React from "react"
+import ReactDom from "react-dom"
+import "./styles.css"
 
 function component() {
-  const content = React.createElement('p',
-    { className: 'content' },
-    'Github Trends',
+  const content = React.createElement(
+    "p",
+    { className: "content" },
+    "Github Trends"
   )
-  const app = React.createElement('div', { className: 'flexcontainer' }, content)
+
+  const app = React.createElement(
+    "div",
+    { className: "flexcontainer" },
+    content
+  )
   return app
 }
 
-ReactDom.render(component(), document.getElementById('root'))
+ReactDom.render(component(), document.getElementById("root"))
